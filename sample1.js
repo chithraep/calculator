5e1e90209699393e820507fa20791801788e5ff2
chithraep <epchithra910@gmail.com>
2023-01-18 16:29:19 +0530

    arithmatic operation

---
+++ sample1.js
@@ -0,0 +1,40 @@
+/*let num =6;
+console.log(num,typeof num) */
+/*let num = String(4)
+console.log(typeof num) */
+/*let num ="1,2,3"
+console.log(typeof num) */
+
+/*
+let num =Number("123")
+console.log(typeof num)
+
+
+*/
+let x
+console.log(x,typeof x)
+x=8
+console.log(x,typeof x)
+x=x+" "
+console.log(x,typeof x)
+x =x -2
+console.log(x,typeof x)
+x=!x
+console.log(x,typeof x)
+
+
+console.log(Boolean(8))
+
+/* let num=4;
+//num = num+1
+
+//num += 2
+
+//let y =num++ //post increment
+let y=++num //pre increment
+console.log(y,num) */
+
+
+let num = 5
+let result =Math.pow(4,3)
+console.log(result)
