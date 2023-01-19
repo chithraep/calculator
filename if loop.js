786d58b64c0c036db1a1ffa8048688849bc7389e
chithraep <epchithra910@gmail.com>
2023-01-19 11:17:31 +0530

    again changed

--- if loop.js
+++ if loop.js
@@ -1,10 +1,15 @@
 let num = 3;
 let num2 = 4;
+let num3 =5;
 
-if(num > num2){
+if(num > num2 && num>num3){
 
 
 console.log("num is greater")
-}else{
-    console.log("bye...")
+}else if(num2>num3){
+    console.log("num2 is greater...")
 }
+else {
+    console.log("num3 is greater")
+}
+
